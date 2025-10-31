dabc659a45267a70c70acbcc53fe103763d3df04
ainyan4645 <merytoku4869ws@gmail.com>
2025-11-01 07:53:58 +0900

    1101ルート・コントローラの調整

--- src/routes/web.php
+++ src/routes/web.php
@@ -59,5 +59,5 @@ Route::middleware(['auth'])->group(function () {
 
     /* プロフィール編集画面 */
     Route::get('/mypage/profile', [MypageController::class, 'mypageEdit'])->name('mypage.edit');
-    Route::put('mypage/profile/update', [MypageController::class, 'update'])->name('mypage.update');
+    Route::put('/mypage/profile/update', [MypageController::class, 'update'])->name('mypage.update');
 });
