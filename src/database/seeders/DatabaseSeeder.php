9a4c3fa39b924cc9fa5c68aba4374fdd556c41e2
ainyan4645 <merytoku4869ws@gmail.com>
2025-11-15 15:54:17 +0900

    1115 DB修正、機能確認用ユーザ追加

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -16,7 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategoriesTableSeeder::class,
             UsersTableSeeder::class,
-            ProfilesTableSeeder::class,
             ProductsTableSeeder::class,
             ProductCategoryTableSeeder::class,
         ]);
--- src/database/seeders/UsersTableSeeder.php
+++ src/database/seeders/UsersTableSeeder.php
@@ -16,19 +16,31 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
+        // サンプル商品出品ユーザー
+        $user1 = User::create([
             'name' => '出品テストユーザー',
             'email' => 'sell@example.com',
             'password' => Hash::make('password'),
         ]);
-
         // 同時にProfile作成
         Profile::create([
-            'user_id' => $user->id,
-            'name' => $user->name, // 名前を一致させる
+            'user_id' => $user1->id,
             'postal_code' => '100-0001',
             'address' => '東京都千代田区千代田1-1',
             'building' => '皇居前ビル',
+        ]);
+
+    // 機能確認用の一般ユーザー
+        $user2 = User::create([
+            'name' => 'テストユーザーA',
+            'email' => 'testA@example.com',
+            'password' => Hash::make('password'),
+        ]);
+        Profile::create([
+            'user_id' => $user2->id,
+            'postal_code' => '150-0001',
+            'address' => '東京都渋谷区神宮前1-1',
+            'building' => '神宮前ビル',
         ]);
     }
 }
